20c66c1d4908750b22727961a80509eae5e7fd9e
vodinhht20 <vodinh2000ht@gmail.com>
2021-09-01 23:36:36 +0700

    create projects DUAN2 by nodejs

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const handlebar = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.engine('hbs', handlebar({
+    extname: '.hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+app.get('/', function(req, res) {
+    res.render('home');
+})
+
+app.listen(port);
