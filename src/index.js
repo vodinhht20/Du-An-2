e54893311ef0a2a061fbdabe8adab5c8b38d171f
vodinhht20 <vodinh2000ht@gmail.com>
2021-09-02 16:45:34 +0700

    render scss to css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('hbs', handlebar({
     extname: '.hbs'
 }));
